acdf2e67a5cecc87f27967eee951914e34699031
lkedves <lkedves@localhost>
2021-02-28 20:18:09 +0100

    GiskardJava - fix TestIOReader JFrame

--- Self/JDK_Ant_Eclipse/Mod/Dust/IO/src/me/giskard/mod/TestIOReader.java
+++ Self/JDK_Ant_Eclipse/Mod/Dust/IO/src/me/giskard/mod/TestIOReader.java
@@ -2,8 +2,6 @@ package me.giskard.mod;
 
 import java.awt.Font;
 import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Insets;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
@@ -11,62 +9,36 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import me.giskard.dust.io.DustIOConsts;
 import me.giskard.dust.io.DustIOUtils;
 
-// https://docs.oracle.com/javase/tutorial/i18n/text/stream.html
+// Based on https://docs.oracle.com/javase/tutorial/i18n/text/stream.html
 
 public class TestIOReader implements DustIOConsts {
-	static class SizeCalculator implements CodepointVisitor {
-		FontMetrics fontM;
-		int size;
-
-		public SizeCalculator(FontMetrics fontM) {
-			this.fontM = fontM;
-		}
-
-		int getSize(String string) {
-			size = 0;
-			DustIOUtils.visitString(string, this);
-			return size;
-		}
-
-		@Override
-		public boolean process(int cp) {
-			size += fontM.charWidth(cp);
-			return true;
-		}
-	};
 	
 	static class ShowString extends JFrame {
 		private static final long serialVersionUID = 1L;
 
 		FontMetrics fontM;
-		String outString;
 
 		ShowString(String target, String title) {
 			super(title);
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			outString = target;
 			
 			Font font = new Font("Monospaced", Font.PLAIN, 36);
-			fontM = getFontMetrics(font);
 			setFont(font);
 
-			int size = new SizeCalculator(fontM).getSize(outString);
-			size += 8;
+			JLabel lbl = new JLabel(target);
+			lbl.setFont(font);
+			
+			getContentPane().add(lbl);
 
-			setSize(size, fontM.getHeight() + 60);
-			setLocation(getSize().width / 2, getSize().height / 2);
+			setLocation(100, 100);
+			
+			pack();
 			setVisible(true);
-		}
-
-		public void paint(Graphics g) {
-			Insets insets = getInsets();
-			int x = insets.left;
-			int y = insets.top;
-			g.drawString(outString, x + 4, y + fontM.getAscent() + 20);
 		}
 	}
 
